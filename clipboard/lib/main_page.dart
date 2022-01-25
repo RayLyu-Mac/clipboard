f19771987c11f0d8dd7225f7bd7264fffe73e2b3
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2022-01-24 21:20:30 -0500

    a

---
+++ clipboard/lib/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class home_page extends StatefulWidget {
+  home_page({Key? key}) : super(key: key);
+
+  @override
+  State<home_page> createState() => _home_pageState();
+}
+
+class _home_pageState extends State<home_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
